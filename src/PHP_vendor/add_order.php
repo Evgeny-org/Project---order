ff98d951289153c5e1c1c62b65dc0f26c34ae997
gra18032002 <gra18032002@outlook.com>
2024-02-29 02:03:03 +0300

    '29.02.24'

--- src/PHP_vendor/add_order.php
+++ src/PHP_vendor/add_order.php
@@ -2,23 +2,21 @@
 	session_start();
 	require_once 'connect.php';
 
-	// $id = $_SESSION['user']['id'];
-	$id = // крч отсюда начинай как придёшь, яхочу вывести id orders, чтобы знать, правильно ли я иду в my_orders.php
 	$user = $_SESSION['user']['id'];
 	$excel = '../Example.xlsx';
 	$state = mysqli_fetch_assoc(mysqli_query($connect, "SELECT `name` FROM `state` LIMIT 1"));
 
-	// echo $user;
-	// echo $state['name'];
-	// echo $user;
-
-	$_SESSION['order'] = [
-			"id" => 
-			"user" => $user,
-			"excel" => $excel,
-			"state" => $state["name"]
-		];
-
 	mysqli_query($connect, "INSERT INTO `orders` (`id`, `user`, `excel`, `state`, `photos`) VALUES (NULL, '$user', '$excel', '".$state["name"]."', NULL)");
+
+	$id = mysqli_insert_id($connect);
+
+	// $_SESSION['order'] = [
+	// 		"id" => $id,
+	// 		"user" => $user,
+	// 		"excel" => $excel,
+	// 		"state" => $state["name"]
+	// 	];
+
+	
 	header("Location: ../my_orders.php");
 ?>
