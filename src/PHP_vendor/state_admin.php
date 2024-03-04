4399140ea7c4af975a7ff3333573c0e04749af45
gra18032002 <gra18032002@outlook.com>
2024-03-04 16:11:42 +0300

    '04.03.24'

--- src/PHP_vendor/state_admin.php
+++ src/PHP_vendor/state_admin.php
@@ -2,8 +2,8 @@
 	session_start();
 	require_once 'connect.php';
 
-	$state = $_POST['select'];
 	$state_id = $_POST['state_id'];
+	$state = $_POST['myVar'];
 
 	mysqli_query($connect, "UPDATE `orders` SET `state` = '$state' WHERE `orders`.`id` = '$state_id'");
 
